99d65d4835134101c9293c05fb5ce2a2e696b3da
TheJackalTR <aryen.hemvatan@std.yeditepe.edu.tr>
2019-10-11 14:25:29 +0300

    İnitial Commit

---
+++ Koşullu Durumlar/hesap_makinesi.py
@@ -0,0 +1,19 @@
+print("""**************************
+Hesap Makinası Uygulaması
+
+İşlemler;
+
+1. Toplama İşlemi
+
+2. Çıakrma İşlemi
+
+3. Çarpma İşlemi
+
+4. Bölme işlemi
+**************************
+""")
+
+a = int(input("Birinci sayı: "))
+b = int(input("İkinci sayı: "))
+
+işlem = input("İşlemi giriniz: ")
