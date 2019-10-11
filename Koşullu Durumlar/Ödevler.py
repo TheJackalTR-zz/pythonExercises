af35208d936965eec8adb870b150e9acd48b6e2d
TheJackalTR <aryen.hemvatan@std.yeditepe.edu.tr>
2019-10-11 17:58:31 +0300

    İnitial Commit

--- Koşullu Durumlar/Ödevler.py
+++ Koşullu Durumlar/Ödevler.py
@@ -27,4 +27,34 @@ elif b > a and b > c:
 else:
     print("The highest number is: ", c)
 
-# %%
+# %% midterm1, midterm2 and final note's Grade Midterm1 will be effect %30, midterm2 will be affect %30
+# Final will be effect %40 or grade
+"""
+midterm1 = int(input("Enter your midterm 1 exam grade:"))
+midterm2 = int(input("Enter your midterm 2 exam grade:"))
+final = int(input("Enter your final exam grade:"))
+"""
+# letterGrade = midterm1 * 0.30 + midterm2 * 0.30 + final * 0.40
+letterGrade = int(input("Letter grade:"))
+print(letterGrade)
+
+if 100 >= letterGrade >= 90:
+    print("Your letter grade is AA")
+elif 90 > letterGrade >= 85:
+    print("Your letter grade is BA")
+elif 85 > letterGrade >= 80:
+    print("Your letter grade is BB")
+elif 80 > letterGrade >= 75:
+    print("Your letter grade is CB")
+elif 75 > letterGrade >= 70:
+    print("Your letter grade is CC")
+elif 70 > letterGrade >= 65:
+    print("Your letter grade is DC")
+elif 65 > letterGrade >= 60:
+    print("Your letter grade is DD")
+elif 60 > letterGrade >= 55:
+    print("Your letter grade is FD")
+elif 55 > letterGrade:
+    print("Your letter grade is FF")
+else:
+    print("Unrecognizable Grade")
