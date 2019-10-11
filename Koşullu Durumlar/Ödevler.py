d3a485b697293769ef934bc4d1ce2597842b2993
TheJackalTR <aryen.hemvatan@std.yeditepe.edu.tr>
2019-10-12 02:55:55 +0300

    İnitial Commit

--- Koşullu Durumlar/Ödevler.py
+++ Koşullu Durumlar/Ödevler.py
@@ -9,7 +9,7 @@ if BMI < 18.5:
     print("You are thin")
 elif 18.5 <= BMI < 25:
     print("You are in normal weight")
-elif 25 <= BMI <= 30:
+elif 25 <= BMI < 30:
     print("You are overweight")
 else:
     print("You are obese")
@@ -29,13 +29,13 @@ else:
 
 # %% midterm1, midterm2 and final note's Grade Midterm1 will be effect %30, midterm2 will be affect %30
 # Final will be effect %40 or grade
-"""
+
 midterm1 = int(input("Enter your midterm 1 exam grade:"))
 midterm2 = int(input("Enter your midterm 2 exam grade:"))
 final = int(input("Enter your final exam grade:"))
-"""
-# letterGrade = midterm1 * 0.30 + midterm2 * 0.30 + final * 0.40
-letterGrade = int(input("Letter grade:"))
+
+letterGrade = midterm1 * 0.30 + midterm2 * 0.30 + final * 0.40
+
 print(letterGrade)
 
 if 100 >= letterGrade >= 90:
@@ -58,3 +58,46 @@ elif 55 > letterGrade:
     print("Your letter grade is FF")
 else:
     print("Unrecognizable Grade")
+
+# %%
+print("""********************
+you can choose from 
+
+1. Triangle
+
+2. Quadrilateral
+********************
+""")
+
+operation = int(input("Enter an operation:"))
+
+if operation == 1:
+    x = float(input("Enter first side of triangle:"))
+    y = float(input("Enter the second side of triangle:"))
+    z = float(input("Enter the third side of triangle:"))
+    if abs(y - z) < x < (y + z) and \
+            abs(x - z) < y < x + z and \
+            abs(x - y) < z < x + y:
+        print("That's a Triangle")
+        if x == y == z:
+            print("That's an equilateral triangle")
+        elif x == y or y == z or x == z:
+            print("That's an isosceles triangle")
+        else:
+            print("That's a scalar triangle")
+    else:
+        print("That's not a Triangle")
+
+elif operation == 2:
+    a = float(input('Enter the first side of Quadrilateral'))
+    b = float(input('Enter the second side of Quadrilateral'))
+    c = float(input('Enter the third side of Quadrilateral'))
+    d = float(input('Enter the fourth side of Quadrilateral'))
+    if a == b == c == d:
+        print("It's a square")
+    elif a == b and c == d or a == c and b == d or a == d and b == c:
+        print("It's a rectangle")
+    else:
+        print("It's an ordinary rectangle")
+else:
+    print("Unrecognizable operation, please try again")
